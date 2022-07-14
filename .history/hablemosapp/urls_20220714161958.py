c306c845cc7e60639a448e8dbefbc52d73461b5a
¨nicolasbernard01¨ <nicolasemanuelbernard@gmail.com>
2022-07-14 16:39:31 -0500

    Implementacion de mensajes de succes o error...

---
+++ .history/hablemosapp/urls_20220714161958.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+
+    path('login/', login_request, name='login'),
+    path('register/', register_request, name='register'),
+    path('logout/', logout_request, name='logout'),
+    path('editar_perfil/', editar_perfil, name='editar_perfil'),
+    path('user_avatar/<avatar_id>', user_avatar, name='user_avatar'),
+    path('', DebateList.as_view(), name="debate_list"),
+    path('leer_debate/<debate_id>', leer_debate, name='leer_debate'),
+    path('debate/', DebateCreate.as_view(), name="debate_create"),
+    path('debate/editar/<pk>', DebateUpdate.as_view(), name="debate_update"),
+    path('debate/eliminar/<pk>', DebateDelete.as_view(), name="debate_delete"),
+ 
+]
